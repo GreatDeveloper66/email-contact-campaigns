bf6292add049ce36cdeb32cf1699030b9c33160c
GreatDeveloper66 <ashaffe@gmail.com>
2020-04-11 14:53:44 -0500

    Change form variables for email campaign structure

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,27 +1,40 @@
 <template>
   <div>
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
+
+      <b-form-group id="input-group-1" label="Recepient Name:" label-for="input-1">
         <b-form-input
           id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input
-          id="input-2"
           v-model="form.name"
           required
           placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Recepient Title:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.title"
+          required
+          placeholder="Enter Title"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Company:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.company"
+          required
+          placeholder="Enter Company"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="Email:" label-for="input-4">
+        <b-form-input
+          id="input-4"
+          v-model="form.email"
+          required
+          placeholder="Enter Email"
         ></b-form-input>
       </b-form-group>
 
@@ -55,8 +68,10 @@
     data() {
       return {
         form: {
-          email: '',
           name: '',
+          title: '',
+          company: '',
+          email: '',
           food: null,
           checked: []
         },
@@ -74,6 +89,8 @@
         // Reset our form values
         this.form.email = ''
         this.form.name = ''
+        this.form.title = ''
+        this.form.company = ''
         this.form.food = null
         this.form.checked = []
         // Trick to reset/clear native browser form validation state
